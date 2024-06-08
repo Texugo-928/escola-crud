f6bb4253a3290cb9c9ad650a32defc012d6af5f0
Texugo-928 <junior-cmf@live.com>
2024-06-08 20:15:04 -0300

    Criando a Repository

--- src/main/java/com/escola/domain/model/Aluno.java
+++ src/main/java/com/escola/domain/model/Aluno.java
@@ -1,5 +1,105 @@
 package com.escola.domain.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity(name = "tb_aluno")
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 80)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private String data_nascimento;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Endereco> endereco;
+
+    @Column(name = "serie", nullable = false)
+    private String serie;
+
+    @Column(name = "segmento", nullable = false)
+    private String segmento;
+
+    @Column(name = "nome_pai")
+    private String nome_pai;
+
+    @Column(name = "nome_mae")
+    private String nome_mae;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public List<Endereco> getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(List<Endereco> endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+
+    public String getSegmento() {
+        return segmento;
+    }
+
+    public void setSegmento(String segmento) {
+        this.segmento = segmento;
+    }
+
+    public String getNome_pai() {
+        return nome_pai;
+    }
+
+    public void setNome_pai(String nome_pai) {
+        this.nome_pai = nome_pai;
+    }
+
+    public String getNome_mae() {
+        return nome_mae;
+    }
+    public void setNome_mae(String nome_mae) {
+        this.nome_mae = nome_mae;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
--- src/main/java/com/escola/domain/model/Endereco.java
+++ src/main/java/com/escola/domain/model/Endereco.java
@@ -1,5 +1,79 @@
 package com.escola.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_endereco")
 public class Endereco {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "tipo", nullable = false)
+    private String tipo;
+
+    @Column(name = "rua", nullable = false)
+    private String rua;
+
+    @Column(name = "numero", nullable = false)
+    private String numero;
+
+    @Column(name = "cep", nullable = false)
+    private String cep;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+    
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+    
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
